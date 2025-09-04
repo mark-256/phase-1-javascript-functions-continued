8e6cb4777f707eaed0936231ded6198aa4171178
Mark <markjohnguto@gmail.com>
2025-09-04 20:05:38 +0300

    Complete JS functions lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+// Function Declaration with Default Parameter
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// Function Expression with Default Parameter
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+// Function Returning a Function (Closure)
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
